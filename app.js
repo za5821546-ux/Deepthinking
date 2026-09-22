4efb62579e9b11dcfadd4f47f3c50f4dfe72f209
za5821546-ux <za5821546@gmail.com>
2026-09-22 15:15:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,29 @@ app.get('/', (req, res) => {
     res.status(200).send('Server is alive and kicking!');
 });
 
+// ROUTE MỚI: '/app' - Tải script từ GitHub Gist và trả về dạng JavaScript
+app.get('/app', (req, res) => {
+    const scriptUrl = 'https://gist.githubusercontent.com/za5821546-ux/1b3cd9a3dead5347fd88e6dd7a73c4ac/raw/5bd95e9e02b29f1359c47c387e068c4c4893781a/APP.JS';
+
+    https.get(scriptUrl, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            // Đặt Header để trình duyệt hiểu đây là tập tin JavaScript
+            res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
+            res.send(data);
+        });
+
+    }).on('error', (err) => {
+        console.error('[App Script Error] Lỗi khi tải file JS:', err.message);
+        res.status(500).send('console.error("Lỗi khi tải script từ server xa");');
+    });
+});
+
 // 2. ROUTE: '/locate' - Nhận lat, lon, kiểm tra khoảng cách và lưu vào RAM
 app.all('/locate', (req, res) => {
     const lat = parseFloat(req.query.lat || req.body.lat);
