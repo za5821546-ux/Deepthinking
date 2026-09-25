22c1959834d710bd38661264c295ddacb63abc99
za5821546-ux <za5821546@gmail.com>
2026-09-26 00:15:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
 
 // 2. ROUTE: '/app' - Lấy mã HTML/JS từ Gist về và render trực tiếp (Đã fix lỗi iframe trả code/file)
 app.get('/app', (req, res) => {
-    const gistUrl = 'https://gist.githubusercontent.com/za5821546-ux/1b3cd9a3dead5347fd88e6dd7a73c4ac/raw/e283651135a1d74d953aad882ea23bf84e3849df/APP.JS';
+    const gistUrl = 'https://gist.githubusercontent.com/za5821546-ux/1b3cd9a3dead5347fd88e6dd7a73c4ac/raw/77f3be4a19c65ed4d7603ec42f64bbfbad2bd4f2/APP.JS';
 
     https.get(gistUrl, (response) => {
         let htmlData = '';
